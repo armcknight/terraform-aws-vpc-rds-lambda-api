6ca523c6a8bcb5bfce3a2b8b7edfc6ed69b97e4a
Andrew McKnight <andrew@TRSrMBP1.lan>
2022-03-04 11:32:51 -0900

    properly set the acl on the bucket after creating

--- main.tf
+++ main.tf
@@ -29,7 +29,10 @@ resource "random_pet" "lambda_bucket_name" {
 
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
-
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
+  bucket = random_pet.lambda_bucket_name.id
+  acl = "private"
 }
