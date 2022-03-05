69cafe6084f6a16e752b147dddc33d955197f336
Andrew McKnight <andrew@TRSrMBP1.lan>
2022-03-05 09:10:04 -0900

    add query param to lambda function and api endpoint

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -4,13 +4,17 @@ import json
 # this function can be named anything, just make sure to supply it to the terraform instruction aws_lambda_function.handler
 def handler(event, context):
     json_region = os.environ['AWS_REGION']
+    if event.queryStringParameters is not None and event.queryStringParameters['Name'] is not None:
+        responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+    else:
+        responseMessage = 'Hello, world!'
     return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
         },
-        "body": json.dumps({
-            "Region ": json_region,
-            "message": "Hello, world!",
+        'body': json.dumps({
+            'Region ': json_region,
+            'message': responseMessage,
         }),
     }
