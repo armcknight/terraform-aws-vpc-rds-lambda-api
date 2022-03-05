2a7a6d1173203f601a30c9305d9c1df92925f64d
Andrew McKnight <andrew@TRSrMBP1.lan>
2022-03-05 09:18:30 -0900

    debug log the event

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -4,10 +4,11 @@ import json
 # this function can be named anything, just make sure to supply it to the terraform instruction aws_lambda_function.handler
 def handler(event, context):
     json_region = os.environ['AWS_REGION']
-    if event.queryStringParameters is not None and event.queryStringParameters['Name'] is not None:
-        responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
-    else:
-        responseMessage = 'Hello, world!'
+    logger.info(f'{event=}')
+    # if event.queryStringParameters is not None and event.queryStringParameters['Name'] is not None:
+    #     responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+    # else:
+    #     responseMessage = 'Hello, world!'
     return {
         'statusCode': 200,
         'headers': {
@@ -15,6 +16,6 @@ def handler(event, context):
         },
         'body': json.dumps({
             'Region ': json_region,
-            'message': responseMessage,
+            'message': 'Hello, world!',
         }),
     }
